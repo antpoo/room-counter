a503366557e755db4a9fd7e76e0b90d067cf4080
antpoo <1jiangand2@gmail.com>
2024-05-13 09:52:03 -0400

    Started basic HTML page

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
+# importing dependent libraries
 from flask import Flask, render_template
+import os
 
+
+# creating flask app object
+app = Flask(__main__)
+
+# route to home page
+@app.route("/")
+def main():
+    return render_template("home.html")
+
+
+# main code 
+if __name__ == "__main__":
+    
+    # run the flask app
+    app.run(host = "0.0.0.0", port=5000)
